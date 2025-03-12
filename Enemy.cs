f6862fd9156965a0b03fd4dd64da0aaacef81420
Louie Maloney <lpm143@outlook.com>
2025-03-12 12:08:44 +0000

    Added an enemy class

---
+++ Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonExplorer
+{
+    internal class Enemy
+    {
+        public string Name { get; private set; }
+        public int Health { get; private set; }
+    
+
+
+    public Enemy(string name, int health)
+        {
+            Name = name;
+            Health = health;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+            Console.WriteLine($"{Name} took {damage} damage.");
+            if (Health <= 0)
+            {
+                Console.WriteLine($"{Name} has been defeated.");
+            }
+        }
+
+        public void ShowEnemyStats()
+        {
+            Console.WriteLine($"Enemy: {Name}");
+            Console.WriteLine($"Health: {Health}");
+        }
+    }
+}
